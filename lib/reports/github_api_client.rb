b564fa11157d707fc9d8f4ad3dcc57cb92c21b6d
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2017-05-25 19:51:59 -0400

    add logging for user info get requests

--- lib/reports/github_api_client.rb
+++ lib/reports/github_api_client.rb
@@ -1,12 +1,26 @@
 require 'faraday'
 require 'json'
+require 'logger'
 
 module Reports
   User = Struct.new(:name, :location, :public_repos_count)
 
   class GitHubAPIClient
+    attr_reader :logger
+
+    def initialize
+      @logger = Logger.new(STDOUT)
+    end
+
     def user_info(username)
-      response = Faraday.get("https://api.github.com/users/#{username}")
+      url = "https://api.github.com/users/#{username}"
+
+      start_time = Time.now
+      response = Faraday.get(url)
+      duration = Time.now - start_time
+
+      logger.debug '-> %s %s %d (%.3f s)' % [url, 'GET', response.status, duration]
+
       data = JSON.parse(response.body)
       User.new(data['name'], data['location'], data['public_repos'])
     end
